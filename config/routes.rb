cde7f9d57575677cc5d98d04ed5ee9f6a00716d5
Chandresh Pant <cp@cpant.in>
2013-06-09 17:35:32 +0530

    fixed update issue

--- config/routes.rb
+++ config/routes.rb
@@ -69,5 +69,5 @@ Cms::Application.routes.draw do
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id))(.:format)'
 
-  get "/:id" => 'pages#show', as: :page
+  get "/:id" => 'pages#show'
 end
