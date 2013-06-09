06ac67c0d66c6e8089767636f98f30ee41357c0a
Chandresh Pant <cp@cpant.in>
2013-06-09 21:01:24 +0530

    change homepage featured logic

--- app/models/page.rb
+++ app/models/page.rb
@@ -7,6 +7,11 @@ class Page < ActiveRecord::Base
 
   friendly_id :name, use: [:slugged, :history]
 
-  scope :featured, where(featured: true)
+  ## scope :featured, where(featured: true)
+
+  def self.featured
+    #show the pages which are in featured category
+    Category.find_by_name("featured").pages
+  end
 
 end
