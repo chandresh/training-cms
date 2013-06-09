52a2b18572d8ee1913e158e07da8cfe787f17344
Chandresh Pant <cp@cpant.in>
2013-06-09 20:09:23 +0530

    admin session

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-
+  helper_method :admin?
 
   private
 
+  def admin?
+    session[:admin].present? && session[:admin]
+  end
+
   def authenticate_with_basic
     authenticate_or_request_with_http_basic('Administration') do |username, password|
-      username == 'admin' && password == 'secret'
+      session[:admin] = (username == 'admin' && password == 'secret')
     end
   end
 
